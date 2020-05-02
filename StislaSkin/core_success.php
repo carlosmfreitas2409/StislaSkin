434ae6a33c53259fecd8c168e9e331bb80546b65
carlosmfreitas2409 <32655274+carlosmfreitas2409@users.noreply.github.com>
2020-05-02 19:46:20 -0300

    Update core_success.php

--- StislaSkin/core_success.php
+++ StislaSkin/core_success.php
@@ -4,6 +4,8 @@
             title: 'Success!', 
             html: "<?php echo $message; ?>", 
             icon: "success"
+        }).then(function() {
+            window.location = "<?php echo SITE_URL; ?>";
         });
     })
 </script>
