a401493d115c2d6e6801d16b92d8335c9eb920f9
carlosmfreitas2409 <32655274+carlosmfreitas2409@users.noreply.github.com>
2020-05-03 16:49:13 -0300

    Fixed airports file location

--- 3rd Party Modules/airportinfo/core/modules/Airports/Airports.php
+++ 3rd Party Modules/airportinfo/core/modules/Airports/Airports.php
@@ -5,13 +5,13 @@ class Airports extends CodonModule
 	public function index()
 	{
 		$this->set('airports', OperationsData::getAllAirports());
-		$this->show('airport_main.php');
+		$this->show('airports/airport_main.php');
 		
 	}
     public function get_airport() {
 
         $icao = $_GET['icao'];
         $this->set('name', OperationsData::getAirportInfo($icao));
-        $this->show('airport_info.php');
+        $this->show('airports/airport_info.php');
     }
 }
